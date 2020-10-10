6d6f2fd392198f98a97eb7f24387a8cd3347e9d4
Marcelo Vieira <marcelobarbosavieira@gmail.com>
2020-10-10 15:58:32 -0300

    Persistindo os dados usando MySQL

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const tabelas = require('./infraestrutura/tabelas')
+const Tabelas = require('./infraestrutura/tabelas')
 
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log('conectado ao banco de dados')
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+        Tabelas.init(conexao)
+
+        const app = customExpress() 
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
+
