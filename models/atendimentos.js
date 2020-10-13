b919e692746f9ce6351257bbc56a1cec692adb7d
Marcelo Vieira <marcelobarbosavieira@gmail.com>
2020-10-13 11:02:14 -0300

    Refatorando o código

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,19 +1,49 @@
+const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento{
 
-    adiciona(atendimento){
-        const sql = 'INSERT INTO Atendimentos SET ?'
+    adiciona(atendimento, res){
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')        
 
-            if(erro){
-                console.log(erro)
-            }else{
-                console.log(resultados)
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente precisa ter pelo menos 5 caracteres'
             }
+        ]
 
-        })
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros){
+            res.status(400).json(erros)
+        }else{
+
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+
+                if(erro){
+                    res.status(400).json(erro)
+                }else{
+                    res.status(201).json(resultados)
+                }
+    
+            })
+        }        
 
     }
 
